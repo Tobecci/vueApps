c8fe73b609a56052da29076affcc87f44df0a99f
Tobecci <ojinakatochukwu@gmail.com>
2019-08-19 22:16:30 +0100

    setup my vuetify template

--- src/App.vue
+++ src/App.vue
@@ -6,35 +6,31 @@
 </template>
 
 <script>
-import showBlogs from './components/showBlogs.vue';
-import listBlogs from './components/listBlogs.vue';
-import appHeader from './components/appHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    'show-blogs': showBlogs,
-    'list-blogs': listBlogs,
-    'app-header': appHeader,
+
   },
     data () {
     return {
-      blogs: [],
+      //
     }
   },
   computed : {
 
   },
-  created: function(){
-    this.$http.get("https://jsonplaceholder.typicode.com/posts").then(function(data){
-      this.blogs = data.body.slice(0,10);
-    });
+  methods: {
+
   },
-  
-  // filters : {
-  //   'to-uppercase' : function(value){
-  //     return value.toUpperCase();
-  //   },
-  // },
+  created: function(){
+    
+  }, 
+  filters : {
+    'to-uppercase' : function(value){
+      return value.toUpperCase();
+    },
+  },
+  mixins: [],
 }
 </script>
