7ad0840f22953ac7fe899f140cc1d8eaf3a24533
Tobecci <ojinakatochukwu@gmail.com>
2019-08-20 11:27:46 +0100

    hopefully it'll work

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,15 @@
 </template>
 
 <script>
+import showBlogs from './components/showBlogs.vue';
+import listBlogs from './components/listBlogs.vue';
 import appHeader from './components/appHeader.vue';
 
 export default {
   name: 'App',
   components: {
+    'show-blogs': showBlogs,
+    'list-blogs': listBlogs,
     'app-header': appHeader,
   },
     data () {
@@ -21,11 +25,11 @@ export default {
   computed : {
 
   },
-  // created: function(){
-  //   this.$http.get("https://jsonplaceholder.typicode.com/posts").then(function(data){
-  //     this.blogs = data.body.slice(0,10);
-  //   });
-  // },
+  created: function(){
+    this.$http.get("https://jsonplaceholder.typicode.com/posts").then(function(data){
+      this.blogs = data.body.slice(0,10);
+    });
+  },
 
   // filters : {
   //   'to-uppercase' : function(value){
