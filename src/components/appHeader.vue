576794644e25b01786c443a13f06076a163e43b9
Tobecci <ojinakatochukwu@gmail.com>
2019-08-20 11:20:35 +0100

    changed some stuff

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,13 +1,13 @@
 <template>
 	<div>
-		<v-toolbar>
+		<v-toolbar dense>
     <v-toolbar-title>
       My Blog
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-btn flat style="text-decoration: none"><router-link style="text-decoration: none" to="/">Show Blogs</router-link></v-btn>
     <v-btn flat><router-link to="/list">List Blogs</router-link></v-btn>
-    <v-btn flat><router-link to="/add">Add Post</router-link></v-btn>
+    <v-btn flat class=""><router-link to="/add">Add Post</router-link></v-btn>
 
     </v-toolbar>
 	</div>
