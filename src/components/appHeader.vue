f1d79b23257c679a86406584c2abc8b371aa5b43
Tobecci <ojinakatochukwu@gmail.com>
2019-08-20 11:29:19 +0100

    sublime merge is crazy

---
+++ src/components/appHeader.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<v-toolbar>
+    <v-toolbar-title>
+      My Blog
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn flat style="text-decoration: none"><router-link style="text-decoration: none" to="/">Show Blogs</router-link></v-btn>
+    <v-btn flat><router-link to="/list">List Blogs</router-link></v-btn>
+    <v-btn flat><router-link to="/add">Add Post</router-link></v-btn>
+
+    </v-toolbar>
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+
+			}
+		},
+	}
+</script>
