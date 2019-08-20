b0c23f36aa59417b194158d919eb1b31c67c6238
Tobecci <ojinakatochukwu@gmail.com>
2019-08-20 23:29:32 +0100

    setup the real vuetify template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <v-app>
-    <product-list-one></product-list-one>
-    <product-list-two></product-list-two>
+      
   </v-app>
 </template>
 
 <script>
-import productListOne from './components/productListOne.vue';
-import productListTwo from './components/productListTwo.vue';
 export default {
   components: {
-    'product-list-one': productListOne,
-    'product-list-two': productListTwo
+
   },
     data () {
     return {
@@ -28,9 +24,7 @@ export default {
 
   },
   filters : {
-    'to-uppercase' : function(value){
-      return value.toUpperCase();
-    },
+    
   },
   mixins: [],
 }
--- src/store/store.js
+++ src/store/store.js
@@ -5,36 +5,15 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
 	state: {
-		products: [
-        {name: "Banana skin", price: 20},
-        {name: "Tiny star", price: 40},
-        {name: "Green shells", price: 60},
-        {name: "Red shells", price: 80},
-      ]
+
 	},
 	getters: {
-		salesProducts: function(state){
-			var salesProducts = state.products.map(function(product){
-				return {
-					name: "**" + product.name + "**",
-					price: product.price / 2,
-				};
-			});
-		return salesProducts;
-		},
+		
 	},
 	mutations: {
-		reducePrice: function(state){
-			state.products.forEach(function(product){
-				product.price--;
-			});
-		},
+		
 	},
 	actions: {
-		reducePrice: function(context){
-			setTimeout(function(){
-				context.commit('reducePrice');
-			},2000);
-		},
+		
 	},
 });
