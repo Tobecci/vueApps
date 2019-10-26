9f0d5a0038a26fcaf069def602d284050195b936
tobecci <ojinakatochukwu@gmail.com>
2019-10-26 20:53:24 +0100

    added mixins

---
+++ src/components/addBlog.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-container>
+    <v-text-field
+      name="Post title"
+      label="label"
+      id="id"
+      v-model="blog.title"
+    ></v-text-field>
+   <v-textarea
+     label="write post contents here..."
+      v-model="blog.body"
+   ></v-textarea>
+   <v-btn @click="addPost">Add</v-btn>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+    data () {
+    return {
+      blog: {
+        title: "",
+        body: "",
+      },
+    }
+  },
+  computed : {
+
+  },
+  methods: {
+    addPost: function(){
+      this.$http.post("https://tobecci-vue-learn.firebaseio.com/posts.json",this.blog)
+      .then(function(){
+        this.blog = {};
+      });
+    },
+  }
+}
+</script>
